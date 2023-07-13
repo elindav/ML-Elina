420c0bcd77c0c305cb3b3e4959172e08050e7646
elindav <elina.davtyan1.y@tumo.orgv>
2023-07-13 11:52:03 +0400

    Numpy added

---
+++ Session 3/equation.py
@@ -0,0 +1,16 @@
+a = 1
+b = -3
+c = 2
+D = pow(b,2) - 4*a*c
+X = ((-1*b+math.sqrt(D))/2*a
+x = ((-b-math.sqrt(D))/2*a
+
+if D = 0:
+print("1 solution")
+
+if D>0:
+print("2 solutions")
+
+if D<0:
+print ("no solution)
+
